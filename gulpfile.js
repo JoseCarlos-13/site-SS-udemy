8a2af613791771f44dbcabff0d3be8c372a80492
José C <josechcolorado@hotmail.com>
2018-04-07 17:22:30 -0300

    site quase concluido

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
-var gulp   = require ('gulp')
- 	, sass = require ('gulp-sass');
+var gulp   = require ('gulp')       
+	, sass = require ('gulp-sass'),
+	browserSync = require ('browser-sync');
 
 	/*transofrmação de arquivos scss em css*/
 	gulp.task('sass', function(){
@@ -8,7 +9,19 @@ var gulp   = require ('gulp')
 			.pipe(gulp.dest('./src/css/'));
 	})
 
-	/*monitoramento de arquivos scss*/
-	gulp.task('listen', function(){
-		 gulp.watch('./src/sass/**/*.scss', ['sass'])
+	gulp.task('server', function(){
+		browserSync.init({
+			server: {
+				baseDir: 'src'
+			}
+		})
+		
+		/*monitoramento de arquivos scss*/
+		gulp.watch('./src/sass/**/*.scss', ['sass'])
+		gulp.watch('./src/css/*.css').on('change', browserSync.reload)
 	})
+
+	
+	
+
+	

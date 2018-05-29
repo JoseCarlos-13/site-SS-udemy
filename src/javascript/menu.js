e3da648599ee2acd7df7f8ce1a1beb1229d725a8
José C <josechcolorado@hotmail.com>
2018-05-28 21:29:42 -0300

    finalização do menu

--- src/javascript/menu.js
+++ src/javascript/menu.js
@@ -3,8 +3,21 @@
 
 	var btn = document.querySelector('.header-nav__hamburgger');
 	var html = document.querySelector('html');
+	var menu = document.querySelector('#mainMenu');
 	var menuOpened = false;
+	var classMenu = 'menu-opened';
+	
+	html.addEventListener('click', function(e){
+		console.log(this);
+		console.log(e.target);
+	
+	if(e.target === html && menuOpened){
+		closeMenu();
+	}
 
+	})
+
+	
 	btn.addEventListener('click', toggleMenu);
 
 	function toggleMenu(e){
@@ -17,11 +30,16 @@
 
 	function closeMenu(){
 		menuOpened = false;
-		html.classList.remove('menu-opened');
+		html.classList.remove(classMenu);
+		btn.blur();
+		btn.setAttribute('aria-expended', false);
+		menu.setAttribute('aria-expended', false);
 	}
 
 	function openMenu(){
 		menuOpened = true;
-		html.classList.add('menu-opened');
+		html.classList.add(classMenu);
+		btn.setAttribute('aria-expended', true);
+		menu.setAttribute('aria-expended', true);
 	}
 }());

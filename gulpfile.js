058c150af7a7a3154c5e19270387d9f0ef732b97
José C <josechcolorado@hotmail.com>
2018-07-19 20:57:33 -0300

    mudanças finais

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,19 @@
-var gulp   = require ('gulp')       
-	, sass = require ('gulp-sass')
-	, include = require ('gulp-file-include')
-	, autoprefixer = require ('gulp-autoprefixer')
-	, uncss = require ('gulp-uncss')
-	, clean = require ('gulp-clean')
-	, imagemin = require ('gulp-imagemin')
-	, browserSync = require ('browser-sync');
+var gulp   = require ('gulp')/**/       
+	, sass = require ('gulp-sass')/**/
+	, include = require ('gulp-file-include')/*reutiliza codigos e estilos*/
+	, autoprefixer = require ('gulp-autoprefixer')/*inclui os prefixos automaticamente onde precisam-se deles*/
+	, uncss = require ('gulp-uncss')/**/
+	, clean = require ('gulp-clean')/*limpa arquivos de determinada pasta*/
+	, concat = require ('gulp-concat')/*unifica dois ou mais arquivos em um só*/
+	, uglify = require ('gulp-uglify')/*minifica arquivos JS*/
+	, cssnano = require ('gulp-cssnano')/*minifica arquivos CSS*/
+	, rename = require ('gulp-rename')/**/
+	, imagemin = require ('gulp-imagemin')/**/
+	, browserSync = require ('browser-sync')/*abre e atualiza automaticamente o projeto no browser*/;
 
 	/*transofrmação de arquivos scss em css*/
 	gulp.task('sass', function(){
-		gulp.src('./src/sass/**/*.scss')
+	return gulp.src('./src/sass/**/*.scss')
 			.pipe(sass())
 			.pipe(autoprefixer())
 			.pipe(gulp.dest('./dist/css/'));
@@ -22,27 +26,49 @@ var gulp   = require ('gulp')
 
 	/*copiar arquivos de uma pasta(src) para outra(dist)*/
 	gulp.task('copy', ['clean'], function(){
-		gulp.src(['src/components/bootstrap/dist/**/*', 
+	return gulp.src(['src/components/bootstrap/dist/**/*', 
 					'src/components/bootstrap/dist/fonts/**/*', 
 					'src/components/bootstrap/dist/js/**/*',
 					'src/components/font-awesome/css/**/*',
 					'src/components/font-awesome/fonts/**/*',
-					'src/css/**/*', 
-					'src/javascript/**/*'], {"base": 'src'})
+					'src/css/**/*'], {"base": 'src'})
 			.pipe(gulp.dest('dist'))
+	})
+
+	gulp.task('svgmin', function(){
+		return gulp.src(['./src/inc/icons/*.svg', '!src/inc/icons/*.min.svg'])
+				.pipe(imagemin())
+				.pipe(rename({
+					suffix:'.min'
+				}))
+				.pipe(gulp.dest('./src/inc/icons/'))
+
 	})
 
 	gulp.task('imagemin', function(){
 		return gulp.src('./src/imagens/**/*')
 				.pipe(imagemin())
 				.pipe(gulp.dest('./dist/imagens/'))
-				
+	})
 
+	/*unificação e minificação de arquivos JS*/
+	gulp.task('build-js', function(){
+		gulp.src('src/javascript/**/*')
+		.pipe(concat('app.min.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest('./dist/javascript/'))
+	})
+
+	gulp.task('default', ['copy'], function(){
+		gulp.start('uncss', 'imagemin', 'sass', 'build-js')
 	})
 
 	/*copiar o arquivos html com os includes visiveis*/
 	gulp.task('html', function(){
-		return gulp.src('./src/**/*.html')
+		return gulp.src([
+			'./src/**/*.html',
+			'!src/inc/**'
+			])
 			.pipe(include())
 			.pipe(gulp.dest('./dist/'))
 	})
@@ -57,15 +83,20 @@ var gulp   = require ('gulp')
 	})
 
 	/*abrir o projeto atraves do terminal Git*/
-	gulp.task('server', ['uncss', 'imagemin', 'sass', 'copy'], function(){
+	gulp.task('server', function(){
 		browserSync.init({
 			server: {
 				baseDir: 'dist'
 			}
 		})
 		
-		/*monitoramento de arquivos scss*/
+		/*monitoramento de arquivos*/
 		gulp.watch('./src/sass/**/*.scss', ['sass'])
 		gulp.watch('./dist/**/*').on('change', browserSync.reload)
 		gulp.watch('./src/**/*.html', ['html'])
+		gulp.watch('src/javascript/**/*', ['build-js'])
+		gulp.watch([
+			'src/inc/icons/*.svg',
+			'!src/inc/icons/*.min.svg'
+			], ['svgmin'])
 	})
